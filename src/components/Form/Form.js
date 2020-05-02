826a32b4382021565a62e9dce5dfa82de2c0aae8
Mokka Kowalewska <kowalewska.mnk@gmail.com>
2020-05-02 13:34:13 +0200

    feat: build form

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -2,8 +2,13 @@ import React from 'react';
 
 const Form = () => (
     <form>
-        <input placeholder="name" />
+        <input placeholder="name" name="name" />
+        <input placeholder="link" name="link" />
+        <input placeholder="image" name="image" />
+        <textarea placeholder="description" name="description" />
+        <button>add new item</button>
     </form>
+
 );
 
 export default Form;
