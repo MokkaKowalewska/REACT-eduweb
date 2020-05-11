094d16541c919853e75027fbc622b13b16b1041c
Mokka Kowalewska <kowalewska.mnk@gmail.com>
2020-05-11 13:25:38 +0200

    change Form component to class component

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,34 +1,48 @@
-import React from "react";
+import React, { Component } from "react";
 import styles from "./Form.module.scss";
 import Input from "../Input/Input";
 import Button from '../Button/Button';
 import Title from '../Title/Title';
 
-const Form = ({ submitFn }) => (
-  <div className={styles.wrapper}>
-    <Title>Add new twitter account</Title>
-    <form autoComplete="off" className={styles.form} onSubmit={submitFn}>
-      <Input
-        name="name"
-        label="Name"
-        maxLength={30}
-      />
-      <Input
-        name="link"
-        label="Twitter link"
-      />
-      <Input
-        name="image"
-        label="Image"
-      />
-      <Input
-        tag="textarea"
-        name="description"
-        label="Description"
-      />
-      <Button>add new item</Button>
-    </form>
-  </div>
-);
+const types = {
+  twitter: 'twitter',
+  article: 'article',
+  note: 'note',
+}
+
+class Form extends Component {
+  state = {
+    activeOption: types.twitter,
+  }
+
+  render() {
+    return(
+      <div className={styles.wrapper}>
+        <Title>Add new twitter account</Title>
+        <form autoComplete="off" className={styles.form} onSubmit={this.props.submitFn}>
+          <Input
+            name="name"
+            label="Name"
+            maxLength={30}
+          />
+          <Input
+            name="link"
+            label="Twitter link"
+          />
+          <Input
+            name="image"
+            label="Image"
+          />
+          <Input
+            tag="textarea"
+            name="description"
+            label="Description"
+          />
+          <Button>add new item</Button>
+        </form>
+      </div>
+    );
+  }
+}
 
 export default Form;
